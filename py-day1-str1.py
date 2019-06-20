23a0af70f6ca2ea711986ff98673f67780577e9c
louis.lu <louis.lu@docusign.com>
2019-06-20 13:43:12 -0700

    add python string ex1 file

---
+++ py-day1-str1.py
@@ -0,0 +1,7 @@
+name1 = "Michael Jordan"
+name2 = "Kobe Bryant"
+name3 = "Klay Thompson"
+print("{:>30}\n{:>30}\n{:>30}".format(name1, name2, name3))
+
+name4 = input("Enter the fourth name: ")
+print(name4)
